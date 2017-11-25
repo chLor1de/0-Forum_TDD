6b4181b97349e24a2855ff41fbeaa835984bc346
Gabor Susztak <gabor.susztak@gmail.com>
2017-11-25 20:59:59 +0100

    Lesson 12

--- resources/views/threads/create.blade.php
+++ resources/views/threads/create.blade.php
@@ -14,17 +14,35 @@
                             {{ csrf_field() }}
 
                             <div class="form-group">
+                                <label for="channel_id">Choose a Channel:</label>
+                                <select class="form-control" id="channel_id" name="channel_id" required>
+                                    <option>Choose a thread!</option>
+                                    @foreach(App\Channel::all() as $channel)
+                                        <option {{ old('channel_id') == $channel->id ? 'selected' : '' }} value="{{ $channel->id }}">{{ $channel->name }}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+
+                            <div class="form-group">
                                 <label for="title">Title:</label>
-                                <input class="form-control" id="title" type="text" name="title">
+                                <input class="form-control" id="title" type="text" name="title"
+                                       value="{{ old('title') }}" required>
                             </div>
                             <div class="form-group">
                                 <label for="body">Title:</label>
-                                <textarea class="form-control" name="body" id="body" rows="8"></textarea>
+                                <textarea class="form-control" name="body" id="body"
+                                          rows="8" required>{{ old('body') }}</textarea>
                             </div>
-
-                            <button type="submit" class="btn btn-primary">Publish</button>
-
-
+                            <div class="form-group">
+                                <button type="submit" class="btn btn-primary">Publish</button>
+                            </div>
+                            @if(count($errors))
+                                <ul class="alert alert-danger">
+                                    @foreach($errors->all() as $error)
+                                        <li>{{ $error }}</li>
+                                    @endforeach
+                                </ul>
+                            @endif
                         </form>
 
                     </div>

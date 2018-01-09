399afe88a023c0b0979f11f220001a59efc810b5
Gabor Susztak <gabor.susztak@gmail.com>
2018-01-09 12:29:59 +0100

    Lesson 40

--- app/Thread.php
+++ app/Thread.php
@@ -3,7 +3,6 @@
 namespace App;
 
 use App\Filters\ThreadFilters;
-use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 
 class Thread extends Model
@@ -78,4 +77,24 @@ class Thread extends Model
     {
         return $filters->apply($query);
     }
+
+    public function subscribe($userId = null)
+    {
+        $this->subscriptions()->create([
+            'user_id' => $userId ?: auth()->id()
+        ]);
+    }
+    public function unsubscribe($userId = null)
+    {
+        $this->subscriptions()
+            ->where('user_id', $userId ?: auth()->id())
+            ->delete();
+    }
+
+    public function subscriptions()
+    {
+        return $this->hasMany(ThreadSubscription::class);
+    }
+
+
 }
--- tests/Unit/ThreadTest.php
+++ tests/Unit/ThreadTest.php
@@ -4,7 +4,6 @@ namespace Tests\Unit;
 
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Tests\TestCase;
-use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class ThreadTest extends TestCase
 {
@@ -21,12 +20,11 @@ class ThreadTest extends TestCase
     /** @test */
     public function a_thread_can_make_a_string_path()
     {
-           $thread = create('App\Thread');
+        $thread = create('App\Thread');
 
-           $this->assertEquals("/threads/{$thread->channel->slug}/{$thread->id}", $thread->path());
+        $this->assertEquals("/threads/{$thread->channel->slug}/{$thread->id}", $thread->path());
     }
 
-    
 
     /** @test */
     public function a_thread_has_replies()
@@ -34,32 +32,54 @@ class ThreadTest extends TestCase
         $this->assertInstanceOf('Illuminate\Database\Eloquent\Collection', $this->thread->replies);
     }
 
-   /** @test */
-   public function a_thread_has_a_creator()
-   {
+    /** @test */
+    public function a_thread_has_a_creator()
+    {
 
-           $this->assertInstanceOf('App\User', $this->thread->creator);
+        $this->assertInstanceOf('App\User', $this->thread->creator);
     }
 
     /** @test */
     public function a_thread_can_add_a_reply()
     {
-            $this->thread->addReply([
-                'body' => 'Foobar',
-                'user_id' => 1
-            ]);
+        $this->thread->addReply([
+            'body' => 'Foobar',
+            'user_id' => 1
+        ]);
 
-            $this->assertCount(1, $this->thread->replies);
-     }
+        $this->assertCount(1, $this->thread->replies);
+    }
 
-     /** @test */
-     public function a_thread_belongs_to_a_channel()
-     {
-            $thread = create('App\Thread');
-            $this->assertInstanceOf('App\Channel', $thread->channel);
+    /** @test */
+    public function a_thread_belongs_to_a_channel()
+    {
+        $thread = create('App\Thread');
+        $this->assertInstanceOf('App\Channel', $thread->channel);
 
-      }
+    }
+
+    /** @test */
+    public function a_thread_can_be_subscribed_to()
+    {
+        $thread = create('App\Thread');
 
 
-   
+        $thread->subscribe($userId = 1);
+
+        $this->assertEquals(
+            1,
+            $thread->subscriptions()->where('user_id', $userId)->count());
+    }
+
+    /** @test */
+    public function a_thread_can_be_unsubscribed()
+    {
+        $thread = create('App\Thread');
+
+        $thread->subscribe($userId = 1);
+        $thread->unsubscribe($userId);
+
+        $this->assertCount(0, $thread->subscriptions);
+    }
+
 }

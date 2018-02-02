886789fc9308c571e440b609cb52c80c9a4d6f18
Gabor Susztak <gabor.susztak@gmail.com>
2018-02-02 15:19:30 +0100

    Lesson 50

--- tests/Unit/SpamTest.php
+++ tests/Unit/SpamTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Inspections\Spam;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 
@@ -10,7 +11,20 @@ class SpamTest extends TestCase
     use DatabaseMigrations;
     
     /** @test */
-    public function it_validates_spam()
+    public function it_checks_for_invalid_keyword()
     {
+        $spam = new Spam();
+
+        $this->assertFalse($spam->detect('Innocent reply here'));
+        $this->expectException('Exception');
+        $spam->detect('yahoo customer support');
     }
+    /** @test */
+    public function it_checks_for_any_key_being_held_down()
+    {
+           $spam = new Spam();
+           $this->expectException('Exception');
+           $spam->detect('Hello Wrodl aaaaaaaaaaaaaaaaaa');
+     }
+    
 }
